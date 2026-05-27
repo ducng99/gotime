e2f41d4ed4b892197ec154e281cfb78be152c903
ducng99 <tom@tomng.dev>
2026-05-27 15:17:12 +1200

    feat: added predefined constants formats

--- java/java.go
+++ java/java.go
@@ -9,6 +9,28 @@ import (
 
 type JavaTime time.Time
 
+// DateTimeFormatter constants - Java predefined format patterns
+// See https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html
+// Note: Java's predefined formatters are DateTimeFormatter objects built with DateTimeFormatterBuilder,
+// not simple pattern strings. These are the closest pattern equivalents.
+const (
+	DateTimeBasicIsoDate      = `yyyyMMdd`
+	DateTimeIsoLocalDate      = `yyyy-MM-dd`
+	DateTimeIsoOffsetDate     = `yyyy-MM-ddXXX`
+	DateTimeIsoDate           = `yyyy-MM-ddXXX`
+	DateTimeIsoLocalTime      = `HH:mm:ss`
+	DateTimeIsoOffsetTime     = `HH:mm:ssXXX`
+	DateTimeIsoTime           = `HH:mm:ssXXX`
+	DateTimeIsoLocalDateTime  = `yyyy-MM-dd'T'HH:mm:ss`
+	DateTimeIsoOffsetDateTime = `yyyy-MM-dd'T'HH:mm:ssXXX`
+	DateTimeIsoZonedDateTime  = `yyyy-MM-dd'T'HH:mm:ssXXX'['VV']'`
+	DateTimeIsoDateTime       = `yyyy-MM-dd'T'HH:mm:ssXXX'['VV']'`
+	DateTimeIsoOrdinalDate    = `yyyy-DDD`
+	DateTimeIsoWeekDate       = `YYYY-'W'ww-e`
+	DateTimeIsoInstant        = `yyyy-MM-dd'T'HH:mm:ss'Z'`
+	DateTimeRfc1123           = `EEE, d MMM yyyy HH:mm:ss z`
+)
+
 // Format formats using Java's SimpleDateFormat/DateTimeFormatter pattern.
 // See https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html
 // Literal text is enclosed in single quotes. A doubled single quote (”)
--- php/php.go
+++ php/php.go
@@ -9,6 +9,24 @@ import (
 
 type PhpTime time.Time
 
+// DateTimeInterface constants - PHP date format constants
+const (
+	DateTimeATOM            = `Y-m-d\TH:i:sP`
+	DateTimeCOOKIE          = `l, d-M-Y H:i:s T`
+	DateTimeISO8601         = `Y-m-d\TH:i:sO`
+	DateTimeISO8601Expanded = `X-m-d\TH:i:sP`
+	DateTimeRFC822          = `D, d M y H:i:s O`
+	DateTimeRFC850          = `l, d-M-y H:i:s T`
+	DateTimeRFC1036         = `D, d M y H:i:s O`
+	DateTimeRFC1123         = `D, d M Y H:i:s O`
+	DateTimeRFC7231         = `D, d M Y H:i:s \G\M\T`
+	DateTimeRFC2822         = `D, d M Y H:i:s O`
+	DateTimeRFC3339         = `Y-m-d\TH:i:sP`
+	DateTimeRFC3339Extended = `Y-m-d\TH:i:s.vP`
+	DateTimeRSS             = `D, d M Y H:i:s O`
+	DateTimeW3C             = `Y-m-d\TH:i:sP`
+)
+
 // Format formats using PHP's date() format string.
 // See https://www.php.net/manual/en/datetime.format.php
 // Literal characters can be escaped with a backslash.
